758b57064a1a972fb2eaf0c81f38fffcc3c3522b
Matheus Wisch <wisch.pg@outlook.com>
2025-03-04 10:10:31 -0300

    :recycle: Feat: Refactoring payment entities

---
+++ PaymentContext.Tests/Entities/StudentTests.cs
@@ -0,0 +1,16 @@
+﻿using PaymentContext.Domain.Entities;
+
+namespace PaymentContext.Tests.Entities;
+
+[TestClass]
+public class StudentTests
+{
+    [TestMethod]
+    public void AdicionarAssinatura()
+    {
+        var subscription = new Subscription(null);
+        var student = 
+            new Student("Matheus", "Wisch", "02031835505", "matheus@gmail.com");
+        student.AddSubscription(subscription);
+    }
+}
