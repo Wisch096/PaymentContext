85498e8d24bbdec118d588aa8d8dd7e9537512a9
Matheus Wisch <wisch.pg@outlook.com>
2025-03-04 09:33:11 -0300

    :sparkles: Feat: Update the rest of payment entities

--- PaymentContext.Domain/Entities/Payment.cs
+++ PaymentContext.Domain/Entities/Payment.cs
@@ -7,18 +7,27 @@ public abstract class Payment
     public DateTime ExpireDate { get; set; }
     public decimal Total { get; set; }
     public decimal TotalPaid { get; set; }
+    public string Payer { get; set; }
+    public string Document { get; set; }
     public string Address { get; set; }
+    public string Email { get; set; }
 }
 
 public class BoletoPayment : Payment
 {
+    public string BarCode { get; set; }
+    public string BoletoNumber { get; set; }
 }
 
 public class CreditCardPayment : Payment
 {
+    public string CardHolderName { get; set; } 
+    public string CardHolderNumber { get; set; } 
+    public string LastTransactionNumber { get; set; } 
 }
 
 public class PayPalPayment : Payment
 {
+    public string TransactionCode { get; set; }
 }
 
