a87f97b824bfaed3a62f2373a8be233b7e9a0482
Matheus Wisch <wisch.pg@outlook.com>
2025-03-04 09:27:17 -0300

    ✨ Feat: Implemeting payments classes

--- PaymentContext.Domain/Entities/Payment.cs
+++ PaymentContext.Domain/Entities/Payment.cs
@@ -1,6 +1,24 @@
 ﻿namespace PaymentContext.Domain.Entities;
 
-public class Payment
+public abstract class Payment
 {
-    
+    public string Number { get; set; }
+    public DateTime PaidDate { get; set; }
+    public DateTime ExpireDate { get; set; }
+    public decimal Total { get; set; }
+    public decimal TotalPaid { get; set; }
+    public string Address { get; set; }
 }
+
+public class BoletoPayment : Payment
+{
+}
+
+public class CreditCardPayment : Payment
+{
+}
+
+public class PayPalPayment : Payment
+{
+}
+
--- PaymentContext.Domain/Entities/Student.cs
+++ PaymentContext.Domain/Entities/Student.cs
@@ -6,5 +6,6 @@ public class Student
     public string LastName { get; set; }
     public string Document { get; set; }
     public string Email { get; set; }
+    public string Address { get; set; }
     public List<Subscription> Subscriptions { get; set; }
 }
