ff4a9889ac4a9026d56718747c5d66b14173e45b
Matheus Wisch <wisch.pg@outlook.com>
2025-03-05 10:16:57 -0300

    :sparkles: Feat: Implement design by contract at addres and document too

--- PaymentContext.Domain/ValueObjects/Addres.cs
+++ PaymentContext.Domain/ValueObjects/Addres.cs
@@ -1,9 +1,21 @@
-﻿using PaymentContext.Shared.ValueObjects;
+﻿using Flunt.Validations;
+using PaymentContext.Shared.ValueObjects;
 
 namespace PaymentContext.Domain.ValueObjects;
 
 public class Addres : ValueObject
 {
+    public Addres(string street, string number, string neighborhood, string city, string state, string country, string zipCode)
+    {
+        Street = street;
+        Number = number;
+        Neighborhood = neighborhood;
+        City = city;
+        State = state;
+        Country = country;
+        ZipCode = zipCode;
+    }
+
     public string Street { get; private set; }
     public string Number { get; private set; }
     public string Neighborhood { get; private set; }
--- PaymentContext.Domain/ValueObjects/Document.cs
+++ PaymentContext.Domain/ValueObjects/Document.cs
@@ -1,4 +1,5 @@
-﻿using PaymentContext.Domain.Enums;
+﻿using Flunt.Validations;
+using PaymentContext.Domain.Enums;
 using PaymentContext.Shared.ValueObjects;
 
 namespace PaymentContext.Domain.ValueObjects;
@@ -9,8 +10,25 @@ public class Document : ValueObject
     {
         Number = number;
         Type = type;
+        
+        AddNotifications(new Contract<Document>()
+            .Requires()
+            .IsTrue(Validate(), "Document.Number", "Document invalid")
+        );
     }
 
     public string Number { get; set; }
     public EDocumentType Type { get; set; }
+
+    private bool Validate()
+    {
+        switch (Type)
+        {
+            case EDocumentType.CNPJ when Number.Length == 14:
+            case EDocumentType.CNPJ when Number.Length == 11:
+                return true;
+            default:
+                return false;
+        }
+    }
 }
