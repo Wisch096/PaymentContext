ac9af264b145e2110fab2e6f3e0f3f60c65344d5
Matheus Wisch <wisch.pg@outlook.com>
2025-03-06 17:50:43 -0300

    :sparkles: Feat: Add fail fast validation

--- PaymentContext.Domain/Commands/CreateBoletoSubscriptionCommand.cs
+++ PaymentContext.Domain/Commands/CreateBoletoSubscriptionCommand.cs
@@ -1,8 +1,11 @@
-﻿using PaymentContext.Domain.Enums;
+﻿using Flunt.Notifications;
+using Flunt.Validations;
+using PaymentContext.Domain.Enums;
+using PaymentContext.Shared.Commands;
 
 namespace PaymentContext.Domain.Commands;
 
-public class CreateBoletoSubscriptionCommand
+public class CreateBoletoSubscriptionCommand : Notifiable<Notification>, ICommand 
 {
     public string FirstName { get; set; }
     public string LastName { get; set; }
@@ -26,4 +29,12 @@ public class CreateBoletoSubscriptionCommand
     public string State { get; private set; }
     public string Country { get; private set; }
     public string ZipCode { get; private set; }
+    public void Validate()
+    {
+        AddNotifications(new Contract<CreateBoletoSubscriptionCommand>()
+            .IsLowerThan(FirstName, 40, "FirstName", "Name should have no more than 40 chars")
+            .IsGreaterThan(LastName, 3, "FirstName", "Name should have at least 3 chars")
+        );
+    }
+    
 }
