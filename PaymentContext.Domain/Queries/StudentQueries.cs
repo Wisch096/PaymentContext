6059db710a75ba8cdef70b69cd2dce00e939a050
Matheus Wisch <wisch.pg@outlook.com>
2025-03-09 18:54:23 -0300

    :sparkles: Feat: Create a expression linq queries

---
+++ PaymentContext.Domain/Queries/StudentQueries.cs
@@ -0,0 +1,11 @@
+﻿using System.Linq.Expressions;
+
+namespace PaymentContext.Domain.Queries;
+
+public static class StudentQueries
+{
+    public static Expression<Func<Entities.Student, bool>> GetStudent(string document)
+    {
+        return x => x.Document.Number == document;
+    }
+}
